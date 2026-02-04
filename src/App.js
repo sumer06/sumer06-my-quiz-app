48c30c20395f353d9fd5a7e85034495127196b9d
sumer06 <estelacaravaca@gmail.com>
2026-02-04 09:56:13 +0100

    Demo CI/CD - ESTELA NUEVO TITULO

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> MI QUIZ APP - CI/CD FUNCIONA</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React + Amplify Gen 1 + GitHub
         </p>
         <a
           className="App-link"
